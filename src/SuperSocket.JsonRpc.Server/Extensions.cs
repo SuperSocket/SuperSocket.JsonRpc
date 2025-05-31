d7a98186e9f660bb4c8b66a1382ed09acfe702b8
Kerry Jiang <kerry-jiang@hotmail.com>
2025-05-30 21:31:57 -0700

    added SuperSocket.JsonRpc.Server

---
+++ src/SuperSocket.JsonRpc.Server/Extensions.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using NRPC.Abstractions;
+using NRPC.Abstractions.Metadata;
+using NRPC.SuperSocket.Server;
+using SuperSocket.ProtoBase;
+using SuperSocket.Server.Abstractions.Host;
+
+namespace SuperSocket.JsonRpc.Server;
+
+public static class Extensions
+{
+    public static ISuperSocketHostBuilder<JsonRpcRequest> UseJsonRpcServer<TServiceContract, TService>(this ISuperSocketHostBuilder<JsonRpcRequest> hostBuilder)
+        where TServiceContract : class
+        where TService : class, TServiceContract
+    {
+        return hostBuilder
+            .UseNRPCService<JsonRpcRequest, JsonRpcResponse, TServiceContract, TService>()
+            .UsePipelineFilter<JsonRpcRequestPipelineFilter>()
+            .UsePackageDecoder<JsonRpcRequestDecoder>()
+            .ConfigureServices((ctx, services) =>
+            {
+                services.AddSingleton<IPackageEncoder<JsonRpcResponse>, JsonRpcResponseEncoder>();
+                services.AddSingleton<JsonRpcResponseDecoder>();
+                services.AddSingleton<IExpressionConverter, JsonElementExpressionConverter>();
+                services.AddSingleton<IRpcCallingAdapter, JsonRpcCallerAdapter>();
+            });
+    }
+}
